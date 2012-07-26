d71276d140b5a9ecb2855c1fe45ceaaab8d797c0
Nietzsche <thomasalwyndavis@gmail.com>
2012-07-26 19:58:42 +1000

    cleaned up

--- bin/wc3js.js
+++
@@ -1,4 +0,0 @@
-var path = require('path');
-var fs = require('fs');
-var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
-require(lib + '/main.js');
--- example/example.js
+++ example/example.js
@@ -2,4 +2,5 @@ var wc3js = require('../lib/wc3js.js');
 
 var results = wc3js.validate({
 	file: 'demo.html'
+	//file: 'http://html5boilerplate.com/'
 });
