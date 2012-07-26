e714fbe0224552d2a772c43540bc1a62266ca0a0
Nietzsche <thomasalwyndavis@gmail.com>
2012-07-26 19:48:36 +1000

    work in progress

---
+++ bin/wc3js.js
@@ -0,0 +1,4 @@
+var path = require('path');
+var fs = require('fs');
+var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+require(lib + '/main.js');
---
+++ example/example.js
@@ -0,0 +1,5 @@
+var wc3js = require('../lib/wc3js.js');
+
+var results = wc3js.validate({
+	file: 'demo.html'
+});
