1addb56137035d453596e517b3013c6a0a52ef30
Thomas Davis <thomasalwyndavis@gmail.com>
2017-06-07 18:30:37 +1000

    Revert "Revert "Update dependencies""
    This reverts commit 7b4103a137b1d09b86a450a5c262a7c480b13062.

--- example/index.js
+++ example/index.js
@@ -7,8 +7,8 @@ var results = w3cjs.validate({
 	// input: myBuffer,
 	output: 'json', // Defaults to 'json', other option includes html
 	// proxy: 'http://proxy:8080', // Default to null
-	callback: function (res) {
-		console.log(res);
+	callback: function (error, res) {
+		console.log(error || res);
 		// depending on the output type, res will either be a json object or a html string
 	}
 });
